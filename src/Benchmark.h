b24297b09a82e029402d448f9b4b8c180174d744
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2016-09-18 16:05:57 +0300

    * made code beautiful

--- src/Benchmark.h
+++ src/Benchmark.h
@@ -27,17 +27,17 @@ static inline std::size_t getMemory(){
 	int result = -1;
 	char line[128];
 
-    while (fgets(line, 128, file) != NULL){
-        if (strncmp(line, "VmSize:", 7) == 0){
-        	// This assumes that a digit will be found and the line ends in " Kb".
+	while (fgets(line, 128, file) != NULL){
+		if (strncmp(line, "VmSize:", 7) == 0){
+			// This assumes that a digit will be found and the line ends in " Kb".
 			int i = strlen(line);
 			const char* p = line;
 			while (*p <'0' || *p > '9') p++;
 			line[i-3] = '\0';
 			result = atoi(p);
-            break;
-        }
-    }
+			break;
+		}
+	}
     fclose(file);
     return result;
 }
